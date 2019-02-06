7c5273ef06fa5f368504e7dfaa06a3b709cc8df7
ahmed-fatouh <39876806+ahmed-fatouh@users.noreply.github.com>
2019-02-06 23:24:06 +0200

    Add HomeController with Index action and view
    Add ability to utilize built-in TagHelpers by adding reference in _ViewImports

---
+++ ConfiguringAspNetCoreApps/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConfiguringAspNetCoreApps.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            Dictionary<string, string> details = new Dictionary<string, string>()
+            {
+                ["Message"] = "This is the Index action"
+            };
+            return View(details);
+        }
+
+
+    }
+}
