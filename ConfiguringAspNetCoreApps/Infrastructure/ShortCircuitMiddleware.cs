8c247a577e74e2cd271654dba6c51bd47219ad2d
ahmed-fatouh <39876806+ahmed-fatouh@users.noreply.github.com>
2019-02-09 17:15:36 +0200

    Add a short-circuiting middleware to the app pipeline

---
+++ ConfiguringAspNetCoreApps/Infrastructure/ShortCircuitMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConfiguringAspNetCoreApps.Infrastructure
+{
+    public class ShortCircuitMiddleware
+    {
+        private RequestDelegate nextDelegate;
+
+        public ShortCircuitMiddleware(RequestDelegate next)
+        {
+            nextDelegate = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if(context.Request.Headers["User-Agent"].Any(h => h.ToLower().Contains("edge")))
+            {
+                context.Response.StatusCode = StatusCodes.Status403Forbidden;
+            }
+            else
+            {
+                await nextDelegate.Invoke(context);
+            }
+        }
+    }
+}
