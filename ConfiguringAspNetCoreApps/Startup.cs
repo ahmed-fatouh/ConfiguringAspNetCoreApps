4e385fe6ad1d192bf284a0df8979bb0da4cf97ac
ahmed-fatouh <39876806+ahmed-fatouh@users.noreply.github.com>
2019-02-09 23:09:09 +0200

    Enable static content middleware

--- ConfiguringAspNetCoreApps/Startup.cs
+++ ConfiguringAspNetCoreApps/Startup.cs
@@ -32,7 +32,7 @@ namespace ConfiguringAspNetCoreApps
             {
                 app.UseExceptionHandler("/Home/Error");
             }
-            
+            app.UseStaticFiles();
             app.UseMvc(route =>
                 route.MapRoute(name: "default",
                                template: "{controller=Home}/{action=Index}/{id?}"));
