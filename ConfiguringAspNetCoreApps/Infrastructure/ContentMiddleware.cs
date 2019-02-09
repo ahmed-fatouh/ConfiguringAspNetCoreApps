ed84027dbd48a83acfd0ebb41f0b70db564200cc
ahmed-fatouh <39876806+ahmed-fatouh@users.noreply.github.com>
2019-02-09 16:59:04 +0200

    Add a content-generating middleware to the app pipeline

---
+++ ConfiguringAspNetCoreApps/Infrastructure/ContentMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfiguringAspNetCoreApps.Infrastructure
+{
+    public class ContentMiddleware
+    {
+        private RequestDelegate nextDelegate;
+        private UpTimeService upTime;
+
+        public ContentMiddleware(RequestDelegate next, UpTimeService up)
+        {
+            nextDelegate = next;
+            upTime = up;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Path.ToString().ToLower() == "/middleware")
+            {
+                await context.Response.WriteAsync(
+                        $"This is from the content middleware. App is running for {upTime.Uptime}ms",
+                        Encoding.UTF8);
+            }
+            else
+            {
+                await nextDelegate.Invoke(context);
+            }
+        }
+    }
+}
--- ConfiguringAspNetCoreApps/Startup.cs
+++ ConfiguringAspNetCoreApps/Startup.cs
@@ -17,13 +17,14 @@ namespace ConfiguringAspNetCoreApps
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<UpTimeService>();
-            services.AddMvc();
+            //services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseMvcWithDefaultRoute();
+            app.UseMiddleware<ContentMiddleware>();
+            //app.UseMvcWithDefaultRoute();
         }
     }
 }
