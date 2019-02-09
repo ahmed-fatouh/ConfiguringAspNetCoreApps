70d53d68c785891da2215a4aa96685eff086fb57
ahmed-fatouh <39876806+ahmed-fatouh@users.noreply.github.com>
2019-02-09 14:35:39 +0200

    Add a Singleton service to measure the app's uptime

--- ConfiguringAspNetCoreApps/Controllers/HomeController.cs
+++ ConfiguringAspNetCoreApps/Controllers/HomeController.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ConfiguringAspNetCoreApps.Infrastructure;
 
 namespace ConfiguringAspNetCoreApps.Controllers
 {
     public class HomeController : Controller
     {
+        private UpTimeService timer;
+
+        public HomeController(UpTimeService _timer)
+        {
+            timer = _timer;
+        }
+
         public ViewResult Index()
         {
             Dictionary<string, string> details = new Dictionary<string, string>()
             {
-                ["Message"] = "This is the Index action"
+                ["Message"] = "This is the Index action",
+                ["UpTime"] = $"{timer.Uptime} ms"
             };
             return View(details);
         }
---
+++ ConfiguringAspNetCoreApps/Infrastructure/UpTimeService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConfiguringAspNetCoreApps.Infrastructure
+{
+    public class UpTimeService
+    {
+        private Stopwatch timer = new Stopwatch();
+
+        public long Uptime => timer.ElapsedMilliseconds;
+
+        public UpTimeService()
+        {
+            timer.Start();
+        }
+    }
+}
