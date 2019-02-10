9bef56dd22361e80f32999ae337b2fcb33e3ddd3
ahmed-fatouh <39876806+ahmed-fatouh@users.noreply.github.com>
2019-02-10 22:05:12 +0200

    Create Configuration methods specific for Development Environement in Startup class.

--- ConfiguringAspNetCoreApps/Startup.cs
+++ ConfiguringAspNetCoreApps/Startup.cs
@@ -25,6 +25,12 @@ namespace ConfiguringAspNetCoreApps
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<UpTimeService>();
+            services.AddMvc(opts => opts.RespectBrowserAcceptHeader = true);
+        }
+
+        public void ConfigureDevelopmentServices(IServiceCollection services)
+        {
+            services.AddSingleton<UpTimeService>();
             services.AddMvc();
         }
 
@@ -37,20 +43,20 @@ namespace ConfiguringAspNetCoreApps
                 app.UseMiddleware<BrowserTypeMiddleware>();
                 app.UseMiddleware<ShortCircuitMiddleware>();
             }
-
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseStatusCodePages();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
+ 
+            app.UseExceptionHandler("/Home/Error");
             app.UseStaticFiles();
             app.UseMvc(route =>
                 route.MapRoute(name: "default",
                                template: "{controller=Home}/{action=Index}/{id?}"));
+        }
+
+        public void ConfigureDevelopment(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
